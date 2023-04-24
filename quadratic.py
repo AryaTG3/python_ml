ef6f7d67f8e690b95e6d2be0b5fd7a8cc7f51adb
AryaTG3 <aryatg95@gmail.com>
2023-04-24 17:09:09 +0530

    impliment quadratic equation

---
+++ quadratic.py
@@ -0,0 +1,9 @@
+a=int(input("enter a coefficnt for x2:"))
+b=int(input("enter a coefficient for x:"))
+c=int(input("enter a coefficient for x0:"))
+d=((b**2)-(4*a*c))**0.51
+e=(-b+d)/(2*a)
+f=(-b-d)/(2*a)
+print(e)
+print(f)
+
