d710062ce211e96e8278f3ad63197be493564ee7
AryaTG3 <aryatg95@gmail.com>
2023-04-25 08:36:08 +0530

    problems

---
+++ 33.py
@@ -0,0 +1,27 @@
+i=1
+while i<=10:
+    i=i+1
+    print(i)
+print("end")
+print("invalid")
+dict={"minnu":1,"manu":3,"minnu":7}
+print(dict)
+print(dict["minnu"])
+
+
+secret_word="deer"
+guss=""
+guss_count=3
+
+while guss!= secret_word:
+    if guss_count<3:
+        guss = input("enter he guss:")
+        print("guss is not correct")
+    else:
+        guss==secret_word
+print("win")
+
+
+
+
+
