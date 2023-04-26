4dc7c12ef034e6d56acf43a59e4dd76c18750705
AryaTG3 <aryatg95@gmail.com>
2023-04-26 11:59:50 +0530

    leap year problem

---
+++ new.py
@@ -0,0 +1,4 @@
+#sdlc software development life cycle
+item_name=input("enter the item name:")
+
+
--- pattern.py
+++ pattern.py
@@ -1,15 +1,45 @@
+#for i in range (1,5):
+   # for j in range (1,4):
+        #print("*",end="")
+    #print()
+
+
+#for i in range (1,5):
+    #for j in range (1,4):
+        #print(i,end="")
+    #print()
+
+#for i in range (1,5):
+    #for j in range (1,4):
+        #print(j,end="")
+    #print()
+
+
 for i in range (1,5):
-    for j in range (1,4):
-        print("*",end="")
+    for j in range(i):
+       print(j+1,end="")
     print()
 
 
 for i in range (1,5):
-    for j in range (1,4):
+    for j in range(i):
+       print("*",end="")
+    print()
+
+#for i in range (1,5):
+    #for j in range(5-i):
+    #   print(i,end="")
+    #print()
+
+for i in range(1, 5):
+    for j in range(i,5):
         print(i,end="")
     print()
 
-for i in range (1,5):
-    for j in range (1,4):
-        print(j,end="")
+for i in range(1,7):
+    for j in range (1,7):
+        if j==1 or i==6 or i==j:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
     print()
