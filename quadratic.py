98fc5d95e899700b1d221acab4bf8c660c8a0d73
AryaTG3 <aryatg95@gmail.com>
2023-04-26 10:09:10 +0530

    leap year problem

--- quadratic.py
+++ quadratic.py
@@ -1,10 +1,12 @@
-a=int(input("enter a coefficnt for x2:"))
-b=int(input("enter a coefficient for x:"))
-c=int(input("enter a coefficient for x0:"))
-d=((b**2)-(4*a*c))**0.51
-e=(-b+d)/(2*a)
-f=(-b-d)/(2*a)
-print(e)
-print(f)
-print("the roots are",e,f)
+a="giraff"
+guss = input("enter your guss:")
+n=0
+while a== guss :
+    n+=1
+    if n==2:
+        break
+    else:
+        continue
+print("you win")
+
 
